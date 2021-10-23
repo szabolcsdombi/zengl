067e0ccbd0b30115653867a8d2a5659698b44b60
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-10-23 16:40:23 +0300

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as readme:
 
 setup(
     name='zengl',
-    version='0.2.1',
+    version='0.2.2',
     py_modules=['_zengl'],
     data_files=[('.', ['zengl.pyi'])],
     ext_modules=[ext],
