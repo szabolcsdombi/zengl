2a3c1a743540149ffc56ccbb128b41952ff61dad
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-10-24 08:26:20 +0300

    use constant as default

--- _zengl.py
+++ _zengl.py
@@ -218,7 +218,7 @@ def settings(primitive_restart, point_size, line_width, front_face, cull_face, c
     res = [bool(primitive_restart), float(point_size), float(line_width), FRONT_FACE[front_face], CULL_FACE[cull_face], color_mask]
 
     if depth is True or depth is False:
-        res.extend([depth, depth, COMPARE_FUNC['less']])
+        res.extend([depth, depth, 0x0201])
 
     else:
         res.extend([bool(depth['test']), bool(depth['write']), COMPARE_FUNC[depth['func']]])
