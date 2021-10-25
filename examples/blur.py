c25dd8a51d9b91d1e528a62942ff8eb22f770a17
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-10-25 17:53:58 +0300

    better blur example

--- examples/blur.py
+++ examples/blur.py
@@ -1,54 +1,90 @@
+import numpy as np
 import zengl
+from objloader import Obj
 
 from window import Window
 
+
+def kernel(s):
+    x = np.arange(-s, s + 1)
+    y = np.exp(-x * x / (s * s / 4))
+    y /= y.sum()
+    v = ', '.join(f'{t:.8f}' for t in y)
+    # from matplotlib import pyplot as plt
+    # plt.plot(x, y)
+    # plt.show()
+    return f'const int N = {s * 2 + 1};\nfloat coeff[N] = float[]({v});'
+
+
 window = Window(1280, 720)
 ctx = zengl.instance(zengl.context())
+ctx.files['kernel'] = kernel(19)
+
+image = ctx.image(window.size, 'rgba8unorm')
+depth = ctx.image(window.size, 'depth24plus')
 
 temp = ctx.image(window.size, 'rgba8unorm')
-image = ctx.image(window.size, 'rgba8unorm')
-temp.clear_value = (1.0, 1.0, 1.0, 1.0)
+output = ctx.image(window.size, 'rgba8unorm')
 
-triangle = ctx.renderer(
+image.clear_value = (0.2, 0.2, 0.2, 1.0)
+
+model = Obj.open('examples/data/monkey.obj').pack('vx vy vz nx ny nz')
+vertex_buffer = ctx.buffer(model)
+
+uniform_buffer = ctx.buffer(size=80)
+
+monkey = ctx.renderer(
     vertex_shader='''
         #version 330
 
-        out vec3 v_color;
+        layout (std140) uniform Common {
+            mat4 mvp;
+        };
 
-        vec2 positions[3] = vec2[](
-            vec2(0.0, 0.8),
-            vec2(-0.6, -0.8),
-            vec2(0.6, -0.8)
-        );
+        layout (location = 0) in vec3 in_vert;
+        layout (location = 1) in vec3 in_norm;
 
-        vec3 colors[3] = vec3[](
-            vec3(1.0, 0.0, 0.0),
-            vec3(0.0, 1.0, 0.0),
-            vec3(0.0, 0.0, 1.0)
-        );
+        out vec3 v_norm;
 
         void main() {
-            gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
-            v_color = colors[gl_VertexID];
+            gl_Position = mvp * vec4(in_vert, 1.0);
+            v_norm = in_norm;
         }
     ''',
     fragment_shader='''
         #version 330
 
-        in vec3 v_color;
+        in vec3 v_norm;
 
         layout (location = 0) out vec4 out_color;
 
         void main() {
-            out_color = vec4(v_color, 1.0);
+            vec3 light = vec3(4.0, 3.0, 10.0);
+            float lum = dot(normalize(light), normalize(v_norm)) * 0.7 + 0.3;
+            out_color = vec4(lum, lum, lum, 1.0);
         }
     ''',
-    framebuffer=[temp],
+    layout=[
+        {
+            'name': 'Common',
+            'binding': 0,
+        },
+    ],
+    resources=[
+        {
+            'type': 'uniform_buffer',
+            'binding': 0,
+            'buffer': uniform_buffer,
+        },
+    ],
+    framebuffer=[image, depth],
     topology='triangles',
-    vertex_count=3,
+    cull_face='back',
+    vertex_buffers=zengl.bind(vertex_buffer, '3f 3f', 0, 1),
+    vertex_count=vertex_buffer.size // zengl.calcsize('3f 3f'),
 )
 
-blur = ctx.renderer(
+blur_x = ctx.renderer(
     vertex_shader='''
         #version 330
 
@@ -69,18 +105,61 @@ blur = ctx.renderer(
 
         layout (location = 0) out vec4 out_color;
 
-        float coeff[25] = float[](
-            1.0 / 273.0, 4.0 / 273.0, 7.0 / 273.0, 4.0 / 273.0, 1.0 / 273.0,
-            4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0,
-            7.0 / 273.0, 26.0 / 273.0, 41.0 / 273.0, 26.0 / 273.0, 7.0 / 273.0,
-            4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0,
-            1.0 / 273.0, 4.0 / 273.0, 7.0 / 273.0, 4.0 / 273.0, 1.0 / 273.0
-        );
+        #include "kernel"
 
         void main() {
             vec3 color = vec3(0.0, 0.0, 0.0);
-            for (int i = 0; i < 25; ++i) {
-                color += texelFetch(Texture, ivec2(gl_FragCoord.xy) + ivec2(i % 5 - 2, i / 5 - 2) * 3, 0).rgb * coeff[i];
+            for (int i = 0; i < N; ++i) {
+                color += texelFetch(Texture, ivec2(gl_FragCoord.xy) + ivec2(i - N / 2, 0), 0).rgb * coeff[i];
+            }
+            out_color = vec4(color, 1.0);
+        }
+    ''',
+    layout=[
+        {
+            'name': 'Texture',
+            'binding': 0,
+        },
+    ],
+    resources=[
+        {
+            'type': 'sampler',
+            'binding': 0,
+            'image': image,
+        },
+    ],
+    framebuffer=[temp],
+    topology='triangles',
+    vertex_count=3,
+)
+
+blur_y = ctx.renderer(
+    vertex_shader='''
+        #version 330
+
+        vec2 positions[3] = vec2[](
+            vec2(-1.0, -1.0),
+            vec2(3.0, -1.0),
+            vec2(-1.0, 3.0)
+        );
+
+        void main() {
+            gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+        }
+    ''',
+    fragment_shader='''
+        #version 330
+
+        uniform sampler2D Texture;
+
+        layout (location = 0) out vec4 out_color;
+
+        #include "kernel"
+
+        void main() {
+            vec3 color = vec3(0.0, 0.0, 0.0);
+            for (int i = 0; i < N; ++i) {
+                color += texelFetch(Texture, ivec2(gl_FragCoord.xy) + ivec2(0, i - N / 2), 0).rgb * coeff[i];
             }
             out_color = vec4(color, 1.0);
         }
@@ -98,19 +177,23 @@ blur = ctx.renderer(
             'image': temp,
         },
     ],
-    framebuffer=[image],
+    framebuffer=[output],
     topology='triangles',
     vertex_count=3,
 )
 
+camera = zengl.camera((3.0, 2.0, 2.0), (0.0, 0.0, 0.5), aspect=window.aspect, fov=45.0)
+uniform_buffer.write(camera)
+
 
 @window.render
 def render():
-    temp.clear()
-    triangle.render()
-    blur.render()
-    temp.blit()
-    image.blit()
+    image.clear()
+    depth.clear()
+    monkey.render()
+    blur_x.render()
+    blur_y.render()
+    output.blit()
 
 
 window.run()
