68b125ea6a892ea9d3523ddd5479b766df31af61
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-10-30 13:09:36 +0300

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Szabolcs Dombi'
 author = 'Szabolcs Dombi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.5'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
