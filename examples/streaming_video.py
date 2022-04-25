05ce232dced2ed12b63084d241487a821e115ee6
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-04-25 09:41:51 +0300

    separate video and webcam examples

--- examples/streaming_video.py
+++ examples/streaming_video.py
@@ -1,17 +1,22 @@
+from itertools import cycle
+
 import imageio
+import numpy as np
 import zengl
 
+import assets
 from window import Window
 
-reader = imageio.get_reader('<video0>')
-it = iter(reader)
-height, width = next(it).shape[:2]
+reader = imageio.get_reader(assets.get('bunny.mp4'))  # https://test-videos.co.uk/bigbuckbunny/mp4-h264
+it = cycle(reader)
 
-window = Window(width, height)
+window = Window(1280, 720)
 ctx = zengl.context()
 
 image = ctx.image(window.size, 'rgba8unorm')
+frame = np.zeros((720, 1280, 4), 'u1')
 
 while window.update():
-    image.write(zengl.rgba(next(it), 'rgb'))
+    frame[::-1, :, 0:3] = next(it)
+    image.write(frame)
     image.blit()
