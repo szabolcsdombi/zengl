0ffcde796cfb651aaa13129385b7b46f4f17e167
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-05-28 19:22:27 +0300

    box grid example

---
+++ examples/box_grid.py
@@ -0,0 +1,126 @@
+import numpy as np
+import zengl
+
+from window import Window
+
+window = Window(1280, 720)
+ctx = zengl.context()
+
+image = ctx.image(window.size, 'rgba8unorm', samples=4)
+depth = ctx.image(window.size, 'depth24plus', samples=4)
+image.clear_value = (0.0, 0.0, 0.0, 1.0)
+
+uniform_buffer = ctx.buffer(size=64)
+
+boxgrid = ctx.pipeline(
+    vertex_shader='''
+        #version 330 core
+
+        layout (std140) uniform Common {
+            mat4 mvp;
+        };
+
+        vec3 vertices[36] = vec3[](
+            vec3(-0.5, -0.5, -0.5),
+            vec3(-0.5, 0.5, -0.5),
+            vec3(0.5, 0.5, -0.5),
+            vec3(0.5, 0.5, -0.5),
+            vec3(0.5, -0.5, -0.5),
+            vec3(-0.5, -0.5, -0.5),
+            vec3(-0.5, -0.5, 0.5),
+            vec3(0.5, -0.5, 0.5),
+            vec3(0.5, 0.5, 0.5),
+            vec3(0.5, 0.5, 0.5),
+            vec3(-0.5, 0.5, 0.5),
+            vec3(-0.5, -0.5, 0.5),
+            vec3(-0.5, -0.5, -0.5),
+            vec3(0.5, -0.5, -0.5),
+            vec3(0.5, -0.5, 0.5),
+            vec3(0.5, -0.5, 0.5),
+            vec3(-0.5, -0.5, 0.5),
+            vec3(-0.5, -0.5, -0.5),
+            vec3(0.5, -0.5, -0.5),
+            vec3(0.5, 0.5, -0.5),
+            vec3(0.5, 0.5, 0.5),
+            vec3(0.5, 0.5, 0.5),
+            vec3(0.5, -0.5, 0.5),
+            vec3(0.5, -0.5, -0.5),
+            vec3(0.5, 0.5, -0.5),
+            vec3(-0.5, 0.5, -0.5),
+            vec3(-0.5, 0.5, 0.5),
+            vec3(-0.5, 0.5, 0.5),
+            vec3(0.5, 0.5, 0.5),
+            vec3(0.5, 0.5, -0.5),
+            vec3(-0.5, 0.5, -0.5),
+            vec3(-0.5, -0.5, -0.5),
+            vec3(-0.5, -0.5, 0.5),
+            vec3(-0.5, -0.5, 0.5),
+            vec3(-0.5, 0.5, 0.5),
+            vec3(-0.5, 0.5, -0.5)
+        );
+
+        out vec3 v_color;
+
+        vec3 hsv2rgb(vec3 c) {
+            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+            return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+        }
+
+        float hash13(vec3 p3) {
+            p3 = fract(p3 * 0.1031);
+            p3 += dot(p3, p3.zyx + 31.32);
+            return fract((p3.x + p3.y) * p3.z);
+        }
+
+        void main() {
+            int N = 10;
+            float px = float(gl_InstanceID % N);
+            float py = float(gl_InstanceID / N % N);
+            float pz = float(gl_InstanceID / N / N % N);
+            vec3 position = vec3(px, py, pz) - float(N - 1) / 2.0;
+            float scale = 0.1;
+            gl_Position = mvp * vec4(position + vertices[gl_VertexID] * scale, 1.0);
+            v_color = hsv2rgb(vec3(hash13(position), 1.0, 0.5));
+        }
+    ''',
+    fragment_shader='''
+        #version 330
+
+        in vec3 v_color;
+
+        layout (location = 0) out vec4 out_color;
+
+        void main() {
+            out_color = vec4(v_color, 1.0);
+        }
+    ''',
+    layout=[
+        {
+            'name': 'Common',
+            'binding': 0,
+        },
+    ],
+    resources=[
+        {
+            'type': 'uniform_buffer',
+            'binding': 0,
+            'buffer': uniform_buffer,
+        }
+    ],
+    framebuffer=[image, depth],
+    topology='triangles',
+    cull_face='back',
+    vertex_count=36,
+    instance_count=1000,
+)
+
+while window.update():
+    t = window.time * 0.5
+    eye = (np.cos(t) * 5.0, np.sin(t) * 5.0, np.sin(t * 0.7) * 2.0)
+    camera = zengl.camera(eye, (0.0, 0.0, 0.0), aspect=window.aspect, fov=45.0)
+    uniform_buffer.write(camera)
+    image.clear()
+    depth.clear()
+    boxgrid.render()
+    image.blit()
