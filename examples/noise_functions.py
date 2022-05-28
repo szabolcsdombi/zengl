4147dd58e3a830c11318840bf51995cba79fd450
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-05-28 19:38:06 +0300

    noise functions

---
+++ examples/noise_functions.py
@@ -0,0 +1,198 @@
+import struct
+
+import zengl
+
+from window import Window
+
+window = Window(1280, 720)
+ctx = zengl.context()
+
+image = ctx.image(window.size, 'rgba8unorm')
+uniform_buffer = ctx.buffer(size=64)
+
+ctx.includes['hash11'] = '''
+    float hash11(float p) {
+        p = fract(p * 0.1031);
+        p *= p + 33.33;
+        p *= p + p;
+        return fract(p);
+    }
+'''
+
+ctx.includes['hash12'] = '''
+    float hash12(vec2 p) {
+        vec3 p3 = fract(vec3(p.xyx) * 0.1031);
+        p3 += dot(p3, p3.yzx + 33.33);
+        return fract((p3.x + p3.y) * p3.z);
+    }
+'''
+
+ctx.includes['hash13'] = '''
+    float hash13(vec3 p3) {
+        p3 = fract(p3 * 0.1031);
+        p3 += dot(p3, p3.zyx + 31.32);
+        return fract((p3.x + p3.y) * p3.z);
+    }
+'''
+
+ctx.includes['hash21'] = '''
+    vec2 hash21(float p) {
+        vec3 p3 = fract(vec3(p) * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yzx + 33.33);
+        return fract((p3.xx + p3.yz) * p3.zy);
+    }
+'''
+
+ctx.includes['hash22'] = '''
+    vec2 hash22(vec2 p) {
+        vec3 p3 = fract(vec3(p.xyx) * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yzx + 33.33);
+        return fract((p3.xx + p3.yz) * p3.zy);
+    }
+'''
+
+ctx.includes['hash23'] = '''
+    vec2 hash23(vec3 p3) {
+        p3 = fract(p3 * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yzx + 33.33);
+        return fract((p3.xx + p3.yz) * p3.zy);
+    }
+'''
+
+ctx.includes['hash31'] = '''
+    vec3 hash31(float p) {
+        vec3 p3 = fract(vec3(p) * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yzx + 33.33);
+        return fract((p3.xxy + p3.yzz) * p3.zyx);
+    }
+'''
+
+
+ctx.includes['hash32'] = '''
+    vec3 hash32(vec2 p) {
+        vec3 p3 = fract(vec3(p.xyx) * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yxz + 33.33);
+        return fract((p3.xxy + p3.yzz) * p3.zyx);
+    }
+'''
+
+ctx.includes['hash33'] = '''
+    vec3 hash33(vec3 p3) {
+        p3 = fract(p3 * vec3(0.1031, 0.1030, 0.0973));
+        p3 += dot(p3, p3.yxz + 33.33);
+        return fract((p3.xxy + p3.yxx) * p3.zyx);
+    }
+'''
+
+ctx.includes['hash41'] = '''
+    vec4 hash41(float p) {
+        vec4 p4 = fract(vec4(p) * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        p4 += dot(p4, p4.wzxy + 33.33);
+        return fract((p4.xxyz + p4.yzzw) * p4.zywx);
+    }
+'''
+
+ctx.includes['hash42'] = '''
+    vec4 hash42(vec2 p) {
+        vec4 p4 = fract(vec4(p.xyxy) * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        p4 += dot(p4, p4.wzxy + 33.33);
+        return fract((p4.xxyz + p4.yzzw) * p4.zywx);
+    }
+'''
+
+ctx.includes['hash43'] = '''
+    vec4 hash43(vec3 p) {
+        vec4 p4 = fract(vec4(p.xyzx)  * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        p4 += dot(p4, p4.wzxy + 33.33);
+        return fract((p4.xxyz + p4.yzzw) * p4.zywx);
+    }
+'''
+
+ctx.includes['hash44'] = '''
+    vec4 hash44(vec4 p4) {
+        p4 = fract(p4  * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        p4 += dot(p4, p4.wzxy + 33.33);
+        return fract((p4.xxyz + p4.yzzw) * p4.zywx);
+    }
+'''
+
+ctx.includes['common'] = '''
+    layout (std140) uniform Common {
+        float time;
+    };
+'''
+
+canvas = ctx.pipeline(
+    vertex_shader='''
+        #version 330
+
+        vec2 positions[3] = vec2[](
+            vec2(-1.0, -1.0),
+            vec2(3.0, -1.0),
+            vec2(-1.0, 3.0)
+        );
+
+        void main() {
+            gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+        }
+    ''',
+    fragment_shader='''
+        #version 330
+
+        #include "common"
+        #include "hash13"
+
+        layout (location = 0) out vec4 out_color;
+
+        void main() {
+            float gray = hash13(vec3(gl_FragCoord.xy, time));
+            out_color = vec4(gray, gray, gray, 1.0);
+        }
+    ''',
+    layout=[
+        {
+            'name': 'Common',
+            'binding': 0,
+        },
+    ],
+    resources=[
+        {
+            'type': 'uniform_buffer',
+            'binding': 0,
+            'buffer': uniform_buffer,
+        },
+    ],
+    framebuffer=[image],
+    topology='triangles',
+    vertex_count=3,
+)
+
+while window.update():
+    image.clear()
+    uniform_buffer.write(struct.pack('f', window.time))
+    canvas.render()
+    image.blit()
+
+'''
+MIT License
+
+Copyright (c) 2014 David Hoskins
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
