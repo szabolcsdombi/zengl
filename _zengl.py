f758ce59cfc54afa7fcafaf07fd0583976f5d998
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-05-30 12:19:21 +0300

    fix uniform buffer array members get valid location

--- _zengl.py
+++ _zengl.py
@@ -330,14 +330,14 @@ def validate(attributes, uniforms, uniform_buffers, vertex_buffers, layout, reso
     attributes = [
         {
             'name': obj['name'].replace('[0]', '[%d]' % i),
-            'location': obj['location'] + i,
+            'location': obj['location'] + i if obj['location'] >= 0 else -1,
         }
         for obj in attributes for i in range(obj['size'])
     ]
     uniforms = [
         {
             'name': obj['name'].replace('[0]', '[%d]' % i),
-            'location': obj['location'] + i,
+            'location': obj['location'] + i if obj['location'] >= 0 else -1,
         }
         for obj in uniforms for i in range(obj['size'])
     ]
