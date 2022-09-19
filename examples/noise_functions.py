dc02ddbd97c4628419b966f8a96ce3580fe1530f
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-09-19 21:40:47 +0300

    fix whitespaces in glsl

--- examples/noise_functions.py
+++ examples/noise_functions.py
@@ -102,7 +102,7 @@ ctx.includes['hash42'] = '''
 
 ctx.includes['hash43'] = '''
     vec4 hash43(vec3 p) {
-        vec4 p4 = fract(vec4(p.xyzx)  * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        vec4 p4 = fract(vec4(p.xyzx) * vec4(0.1031, 0.1030, 0.0973, 0.1099));
         p4 += dot(p4, p4.wzxy + 33.33);
         return fract((p4.xxyz + p4.yzzw) * p4.zywx);
     }
@@ -110,7 +110,7 @@ ctx.includes['hash43'] = '''
 
 ctx.includes['hash44'] = '''
     vec4 hash44(vec4 p4) {
-        p4 = fract(p4  * vec4(0.1031, 0.1030, 0.0973, 0.1099));
+        p4 = fract(p4 * vec4(0.1031, 0.1030, 0.0973, 0.1099));
         p4 += dot(p4, p4.wzxy + 33.33);
         return fract((p4.xxyz + p4.yzzw) * p4.zywx);
     }
