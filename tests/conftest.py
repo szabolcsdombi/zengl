5839ff31d4c82a1748bb04ff8ff7dab4cc50194f
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-02-28 12:43:33 +0200

    fallback to default headless context

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import sys
+
 import pytest
 import zengl
-from glcontext import headless
 
 
 @pytest.fixture
 def ctx():
-    devices = headless.devices()
-    headless.init(device=next(x['device'] for x in devices if 'EGL_MESA_device_software' in x['extensions']))
-    return zengl.context(headless)
+    if sys.platform.startswith('linux'):
+        from glcontext import headless
+        devices = headless.devices()
+        headless.init(device=next(x['device'] for x in devices if 'EGL_MESA_device_software' in x['extensions']))
+        return zengl.context(headless)
+    else:
+        return zengl.context(zengl.loader(headless=True))
