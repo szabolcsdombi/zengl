4538226ab104fce8e401d45dfbfcf3e1e8528217
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-02 19:14:08 +0200

    imgui demo

---
+++ examples/integrate_imgui.py
@@ -0,0 +1,114 @@
+import math
+import struct
+
+import imgui
+import zengl
+from imgui.integrations.pyglet import create_renderer
+from objloader import Obj
+from PIL import Image
+
+import assets
+from window import Window
+
+window = Window()
+
+imgui.create_context()
+imgui.get_io().ini_file_name = b''
+impl = create_renderer(window._wnd)
+
+ctx = zengl.context()
+
+image = ctx.image(window.size, 'rgba8unorm', samples=4)
+depth = ctx.image(window.size, 'depth24plus', samples=4)
+image.clear_value = (1.0, 1.0, 1.0, 1.0)
+
+model = Obj.open(assets.get('box.obj')).pack('vx vy vz nx ny nz tx ty')
+vertex_buffer = ctx.buffer(model)
+
+img = Image.open(assets.get('crate.png')).convert('RGBA')
+texture = ctx.image(img.size, 'rgba8unorm', img.tobytes())
+
+uniform_buffer = ctx.buffer(size=80)
+
+crate = ctx.pipeline(
+    vertex_shader='''
+        #version 450 core
+
+        layout (std140, binding = 0) uniform Common {
+            mat4 mvp;
+            vec3 light;
+        };
+
+        layout (location = 0) in vec3 in_vert;
+        layout (location = 1) in vec3 in_norm;
+        layout (location = 2) in vec2 in_text;
+
+        out vec3 v_vert;
+        out vec3 v_norm;
+        out vec2 v_text;
+
+        void main() {
+            gl_Position = mvp * vec4(in_vert, 1.0);
+            v_vert = in_vert;
+            v_norm = in_norm;
+            v_text = in_text;
+        }
+    ''',
+    fragment_shader='''
+        #version 450 core
+
+        layout (std140, binding = 0) uniform Common {
+            mat4 mvp;
+            vec3 light;
+        };
+
+        layout (binding = 0) uniform sampler2D Texture;
+
+        in vec3 v_vert;
+        in vec3 v_norm;
+        in vec2 v_text;
+
+        layout (location = 0) out vec4 out_color;
+
+        void main() {
+            float lum = clamp(dot(normalize(light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.6 + 0.4;
+            out_color = vec4(texture(Texture, v_text).rgb * lum, 1.0);
+        }
+    ''',
+    resources=[
+        {
+            'type': 'uniform_buffer',
+            'binding': 0,
+            'buffer': uniform_buffer,
+        },
+        {
+            'type': 'sampler',
+            'binding': 0,
+            'image': texture,
+        },
+    ],
+    framebuffer=[image, depth],
+    topology='triangles',
+    cull_face='back',
+    vertex_buffers=zengl.bind(vertex_buffer, '3f 3f 2f', 0, 1, 2),
+    vertex_count=vertex_buffer.size // zengl.calcsize('3f 3f 2f'),
+)
+
+while window.update():
+    x, y = math.sin(window.time * 0.5) * 3.0, math.cos(window.time * 0.5) * 3.0
+    camera = zengl.camera((x, y, 1.5), (0.0, 0.0, 0.0), aspect=window.aspect, fov=45.0)
+
+    ctx.new_frame()
+    uniform_buffer.write(camera)
+    uniform_buffer.write(struct.pack('3f4x', x, y, 1.5), offset=64)
+
+    image.clear()
+    depth.clear()
+    crate.render()
+    image.blit()
+    ctx.end_frame()
+
+    imgui.new_frame()
+    imgui.show_demo_window()
+    imgui.render()
+    impl.render(imgui.get_draw_data())
