8369df82b9b2b49b246891f2ae3578f2c715a9e4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-08 22:26:04 +0200

    headless context for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
+import sys
+
 import pytest
 import zengl
 
 
 @pytest.fixture
 def ctx():
-    return zengl.context(zengl.loader(headless=True))
+    if sys.platform.startswith('linux'):
+        from glcontext import headless
+        devices = headless.devices()
+        headless.init(device=next(x['device'] for x in devices if 'EGL_MESA_device_software' in x['extensions']))
+        return zengl.context(headless)
+    else:
+        return zengl.context(zengl.loader(headless=True))
