8c000ac963ce5e1012aa3fb9a8da0b5e7f9aecba
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-09 13:26:30 +0200

    remove stencil8 test

--- tests/test_image_formats.py
+++ tests/test_image_formats.py
@@ -6,7 +6,7 @@ IMAGE_FORMATS = [
     'r8unorm', 'rg8unorm', 'rgba8unorm', 'r8snorm', 'rg8snorm', 'rgba8snorm', 'r8uint', 'rg8uint',
     'rgba8uint', 'r16uint', 'rg16uint', 'rgba16uint', 'r32uint', 'rg32uint', 'rgba32uint', 'r8sint', 'rg8sint',
     'rgba8sint', 'r16sint', 'rg16sint', 'rgba16sint', 'r32sint', 'rg32sint', 'rgba32sint', 'r16float', 'rg16float',
-    'rgba16float', 'r32float', 'rg32float', 'rgba32float', 'rgba8unorm-srgb', 'stencil8',
+    'rgba16float', 'r32float', 'rg32float', 'rgba32float', 'rgba8unorm-srgb',
     'depth16unorm', 'depth24plus', 'depth24plus-stencil8', 'depth32float',
 ]
 
