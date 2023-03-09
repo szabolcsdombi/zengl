ba369d7ffbe703e604936de9dbd2e84f8d0d8fe9
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-09 17:34:31 +0200

    fix tests

--- tests/test_context_info.py
+++ tests/test_context_info.py
@@ -2,5 +2,14 @@ import zengl
 
 
 def test_context_info(ctx: zengl.Context):
-    assert isinstance(ctx.info, tuple)
-    assert len(ctx.info) == 3
+    assert isinstance(ctx.info, dict)
+
+    assert 'vendor' in ctx.info
+    assert 'renderer' in ctx.info
+    assert 'version' in ctx.info
+    assert 'glsl' in ctx.info
+
+    assert isinstance(ctx.info['vendor'], str)
+    assert isinstance(ctx.info['renderer'], str)
+    assert isinstance(ctx.info['version'], str)
+    assert isinstance(ctx.info['glsl'], str)
