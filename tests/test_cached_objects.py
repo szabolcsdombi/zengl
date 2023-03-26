86e6f75f783c40c3384274c31cbf4d54bed98e0e
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-26 20:44:47 +0300

    test caches

---
+++ tests/test_cached_objects.py
@@ -0,0 +1,62 @@
+import numpy as np
+import zengl
+
+
+def make_pipeline(ctx, framebuffer, viewport, color):
+    return ctx.pipeline(
+        vertex_shader='''
+            #version 330 core
+
+            vec2 positions[3] = vec2[](
+                vec2(0.1, 0.0),
+                vec2(-0.05, 0.086),
+                vec2(-0.05, -0.086)
+            );
+
+            void main() {
+                gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+            }
+        ''',
+        fragment_shader='''
+            #version 330 core
+
+            #include "color"
+
+            layout (location = 0) out vec4 out_color;
+
+            void main() {
+                out_color = vec4(color, 1.0);
+            }
+        ''',
+        framebuffer=framebuffer,
+        viewport=viewport,
+        topology='triangles',
+        vertex_count=3,
+        includes={
+            'color': f'vec3 color = vec3({color[0]:.3f}, {color[1]:.3f}, {color[2]:.3f});',
+        }
+    )
+
+
+def test(ctx: zengl.Context):
+    image = ctx.image((64, 64), 'rgba8unorm')
+    depth = ctx.image((64, 64), 'depth24plus')
+    depth.clear_value = 0.5
+
+    pipeline_1 = make_pipeline(ctx, [image, depth], (0, 0, 32, 32), (0.0, 0.0, 1.0))
+    pipeline_2 = make_pipeline(ctx, [image, depth], (32, 32, 32, 32), (0.0, 0.0, 1.0))
+    pipeline_3 = make_pipeline(ctx, [image, depth], (0, 32, 32, 32), (0.0, 1.0, 0.0))
+    pipeline_4 = make_pipeline(ctx, [image], (32, 0, 32, 32), (0.0, 0.0, 1.0))
+
+    inspect_1 = zengl.inspect(pipeline_1)
+    inspect_2 = zengl.inspect(pipeline_2)
+    inspect_3 = zengl.inspect(pipeline_3)
+    inspect_4 = zengl.inspect(pipeline_4)
+
+    assert inspect_1['vertex_array'] == inspect_2['vertex_array'] == inspect_3['vertex_array'] == inspect_4['vertex_array']
+
+    assert inspect_1['framebuffer'] == inspect_2['framebuffer'] == inspect_3['framebuffer']
+    assert inspect_1['framebuffer'] != inspect_4['framebuffer']
+
+    assert inspect_1['program'] == inspect_2['program'] == inspect_4['program']
+    assert inspect_1['program'] != inspect_3['program']
