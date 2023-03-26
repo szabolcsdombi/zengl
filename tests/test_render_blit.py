407c49a1e899c17d402695286ef67ea523cf41dc
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-26 19:43:48 +0300

    blit viewport

--- tests/test_render_blit.py
+++ tests/test_render_blit.py
@@ -2,7 +2,7 @@ import numpy as np
 import zengl
 
 
-def test(ctx: zengl.Context):
+def test_blit(ctx: zengl.Context):
     temp = ctx.image((64, 64), 'rgba8unorm')
     image = ctx.image((64, 64), 'rgba8unorm')
     pipeline = ctx.pipeline(
@@ -41,6 +41,53 @@ def test(ctx: zengl.Context):
     pixels = np.frombuffer(image.read(), 'u1').reshape(64, 64, 4)
     np.testing.assert_array_equal(pixels[[16, 16, 48, 48], [16, 48, 16, 48]], [
         [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 255, 255],
+    ])
+
+
+def test_blit_viewport(ctx: zengl.Context):
+    temp = ctx.image((64, 64), 'rgba8unorm')
+    image = ctx.image((64, 64), 'rgba8unorm')
+    pipeline = ctx.pipeline(
+        vertex_shader='''
+            #version 330 core
+
+            vec2 positions[3] = vec2[](
+                vec2(0.1, 0.0),
+                vec2(-0.05, 0.086),
+                vec2(-0.05, -0.086)
+            );
+
+            void main() {
+                gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+            }
+        ''',
+        fragment_shader='''
+            #version 330 core
+
+            layout (location = 0) out vec4 out_color;
+
+            void main() {
+                out_color = vec4(0.0, 0.0, 1.0, 1.0);
+            }
+        ''',
+        framebuffer=[temp],
+        topology='triangles',
+        vertex_count=3,
+    )
+
+    ctx.new_frame()
+    temp.clear()
+    image.clear()
+    pipeline.render()
+    temp.blit(image, source_viewport=(16, 16, 32, 32), target_viewport=(0, 0, 32, 32))
+    temp.blit(image, source_viewport=(16, 16, 32, 32), target_viewport=(32, 32, 32, 32))
+    ctx.end_frame()
+    pixels = np.frombuffer(image.read(), 'u1').reshape(64, 64, 4)
+    np.testing.assert_array_equal(pixels[[16, 16, 48, 48], [16, 48, 16, 48]], [
+        [0, 0, 255, 255],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 255, 255],
