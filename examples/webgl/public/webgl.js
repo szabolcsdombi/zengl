8760a6aea9f6d9efcc9a516ff67ae7010435f3a2
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-03-30 22:46:40 +0300

    prorotype a robust single canvas support

--- examples/webgl/public/webgl.js
+++ examples/webgl/public/webgl.js
@@ -1,4 +1,65 @@
-const zenglSymbols = (wasm) => {
+const zenglSetup = (pyodide, canvas) => {
+  const gl = canvas.getContext('webgl2', {
+    alpha: false,
+    depth: false,
+    stencil: false,
+    antialias: false,
+    premultipliedAlpha: false,
+    preserveDrawingBuffer: false,
+    powerPreference: 'high-performance',
+  });
+
+  canvas.addEventListener('mousemove', (evt) => {
+    state.mx = evt.x;
+    state.my = evt.y;
+  });
+
+  canvas.addEventListener('keydown', (evt) => {
+    state.keys.add(evt.key);
+  });
+
+  canvas.addEventListener('keyup', (evt) => {
+    state.keys.delete(evt.key);
+  });
+
+  const state = {
+    width: canvas.width,
+    height: canvas.height,
+    prevMx: 0,
+    prevMy: 0,
+    prevKeys: new Set(),
+    mx: 0,
+    my: 0,
+    keys: new Set(),
+    timeRef: null,
+    timeElapsed: 0,
+    frame: 0,
+  };
+
+  const render = (timestamp) => {
+    state.timeElapsed = (timestamp - state.timeRef) * 1e-3;
+
+    const callback = pyodide.globals.get('render');
+    if (callback !== undefined) {
+      callback();
+    }
+
+    state.prevMx = state.mx;
+    state.prevMy = state.my;
+    state.prevKeys = state.keys;
+    state.keys = new Set(state.keys);
+    state.frame += 1;
+
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame((timestamp) => {
+    state.timeRef = timestamp;
+    requestAnimationFrame(render);
+  });
+
+  const wasm = pyodide._module;
+
   const textEncoder = new TextEncoder('utf-8');
   const textDecoder = new TextDecoder('utf-8');
 
@@ -38,90 +99,32 @@ const zenglSymbols = (wasm) => {
     }
   };
 
-  const windows = new Map();
-  let windowid = 1;
-
   const glo = new Map();
   let glid = 1;
   glo[0] = null;
-  let gl = null;
 
-  return {
-    zengl_create_window(width, height, handler, root) {
-      const canvas = document.createElement('canvas');
-      canvas.width = width;
-      canvas.height = height;
-      canvas.tabIndex = 1;
-      if (root === 0) {
-        document.body.appendChild(canvas);
-      } else {
-        document.getElementById(getString(root)).appendChild(canvas);
-      }
-      const ctx = canvas.getContext('webgl2', {
-        alpha: false,
-        depth: false,
-        stencil: false,
-        antialias: false,
-        premultipliedAlpha: false,
-        preserveDrawingBuffer: false,
-        powerPreference: 'high-performance',
-      });
-      const state = {
-        prevMx: 0,
-        prevMy: 0,
-        prevKeys: new Set(),
-        mx: 0,
-        my: 0,
-        keys: new Set(),
-        timeRef: null,
-        timeElapsed: 0,
-      };
-      canvas.addEventListener('mousemove', (evt) => {
-        state.mx = evt.x;
-        state.my = evt.y;
-      });
-      canvas.addEventListener('keydown', (evt) => {
-        state.keys.add(evt.key);
-      });
-      canvas.addEventListener('keyup', (evt) => {
-        state.keys.delete(evt.key);
-      });
-      const render = (timestamp) => {
-        if (state.timeRef === null) {
-          state.timeRef = timestamp;
-        }
-        state.timeElapsed = (timestamp - state.timeRef) * 1e-3;
-        gl = ctx;
-        wasm.callPyObject(handler, []);
-        state.prevMx = state.mx;
-        state.prevMy = state.my;
-        state.prevKeys = state.keys;
-        state.keys = new Set(state.keys);
-        requestAnimationFrame(render);
-      };
-      requestAnimationFrame(render);
-      const window = windowid++;
-      windows[window] = { canvas, ctx, state };
-      return window;
+  pyodide._module.mergeLibSymbols({
+    zengl_get_frame() {
+      return state.frame;
     },
-    zengl_time_state(window) {
-      const state = windows[window].state;
+    zengl_get_time() {
       return state.timeElapsed;
     },
-    zengl_mouse_state(window, mouse) {
-      const state = windows[window].state;
+    zengl_get_mouse(mouse) {
       wasm.HEAP32[mouse / 4] = state.mx;
       wasm.HEAP32[mouse / 4 + 1] = state.my;
-      wasm.HEAP32[mouse / 4 + 2] = state.mx - state.prevMx;
-      wasm.HEAP32[mouse / 4 + 3] = state.my - state.prevMy;
     },
-    zengl_key_state(window, key) {
-      const state = windows[window].state;
+    zengl_get_mouse_delta(mouse) {
+      wasm.HEAP32[mouse / 4] = state.mx - state.prevMx;
+      wasm.HEAP32[mouse / 4 + 1] = state.my - state.prevMy;
+    },
+    zengl_get_size(size) {
+      wasm.HEAP32[size / 4] = state.width;
+      wasm.HEAP32[size / 4 + 1] = state.height;
+    },
+    zengl_get_key(key) {
       const keyName = getString(key);
       return (state.keys.has(keyName) ? 1 : 0) | (state.prevKeys.has(keyName) ? 2 : 0);
-    },
-    zengl_make_current(window) {
-      gl = windows[window].ctx;
     },
     zengl_glCullFace(mode) {
       gl.cullFace(mode);
@@ -531,5 +534,5 @@ const zenglSymbols = (wasm) => {
     zengl_glVertexAttribDivisor(index, divisor) {
       gl.vertexAttribDivisor(index, divisor);
     },
-  };
+  });
 };
--- examples/webgl/webgl/webgl.c
+++ examples/webgl/webgl/webgl.c
@@ -15,11 +15,12 @@ typedef unsigned char GLubyte;
 typedef char GLchar;
 typedef void * GLsync;
 
-extern void * zengl_create_window(int width, int height, void * handler, const char * root);
-extern void zengl_make_current(void * window);
-extern double zengl_time_state(void * window);
-extern void zengl_mouse_state(void * window, int * mouse);
-extern int zengl_key_state(void * window, const char * key);
+extern int zengl_get_frame();
+extern double zengl_get_time();
+extern void zengl_get_mouse(int * mouse);
+extern void zengl_get_mouse_delta(int * mouse);
+extern void zengl_get_size(int * size);
+extern int zengl_get_key(const char * key);
 
 extern void zengl_glCullFace(GLenum mode);
 extern void zengl_glClear(GLbitfield mask);
@@ -607,6 +608,79 @@ void impl_glVertexAttribDivisor(GLuint index, GLuint divisor) {
     zengl_glVertexAttribDivisor(index, divisor);
 }
 
+typedef struct Window {
+    PyObject_HEAD
+} Window;
+
+typedef struct DefaultLoader {
+    PyObject_HEAD
+} DefaultLoader;
+
+static PyTypeObject * Window_type;
+static PyTypeObject * DefaultLoader_type;
+
+static PyObject * Window_meth_key_pressed(Window * self, PyObject * arg) {
+    if (!PyUnicode_CheckExact(arg)) {
+        return NULL;
+    }
+    int flags = zengl_get_key(PyUnicode_AsUTF8(arg));
+    if ((flags & 1) && (~flags & 2)) {
+        Py_RETURN_TRUE;
+    } else {
+        Py_RETURN_FALSE;
+    }
+}
+
+static PyObject * Window_meth_key_down(Window * self, PyObject * arg) {
+    if (!PyUnicode_CheckExact(arg)) {
+        return NULL;
+    }
+    int flags = zengl_get_key(PyUnicode_AsUTF8(arg));
+    if (flags & 1) {
+        Py_RETURN_TRUE;
+    } else {
+        Py_RETURN_FALSE;
+    }
+}
+
+static PyObject * Window_meth_key_release(Window * self, PyObject * arg) {
+    if (!PyUnicode_CheckExact(arg)) {
+        return NULL;
+    }
+    int flags = zengl_get_key(PyUnicode_AsUTF8(arg));
+    if ((~flags & 1) & (flags & 2)) {
+        Py_RETURN_TRUE;
+    } else {
+        Py_RETURN_FALSE;
+    }
+}
+
+static PyObject * Window_get_size(Window * self, void * closure) {
+    int size[2];
+    zengl_get_size(size);
+    return Py_BuildValue("(ii)", size[0], size[1]);
+}
+
+static PyObject * Window_get_aspect(Window * self, void * closure) {
+    int size[2];
+    zengl_get_size(size);
+    return PyFloat_FromDouble((double)size[0] / (double)size[1]);
+}
+
+static PyObject * Window_get_mouse(Window * self, void * closure) {
+    int mouse[2];
+    zengl_get_mouse(mouse);
+    return Py_BuildValue("(ii)", mouse[0], mouse[1]);
+}
+
+static PyObject * Window_get_time(Window * self, void * closure) {
+    return PyFloat_FromDouble(zengl_get_time());
+}
+
+static PyObject * Window_get_frame(Window * self, void * closure) {
+    return PyLong_FromLong(zengl_get_frame());
+}
+
 PyObject * lookup;
 
 static void fn(const char * name, void * ptr) {
@@ -622,117 +696,6 @@ static PyObject * load_opengl_function(PyObject * self, PyObject * arg) {
     return res;
 }
 
-typedef struct Window {
-    PyObject_HEAD
-    PyObject * size;
-    PyObject * aspect;
-    PyObject * mouse;
-    PyObject * mouse_delta;
-    PyObject * render;
-    void * window;
-    double time;
-    int mouse_x;
-    int mouse_y;
-    int mouse_dx;
-    int mouse_dy;
-    int frame;
-} Window;
-
-static PyTypeObject * Window_type;
-
-static Window * meth_window(PyObject * self, PyObject * args, PyObject * kwargs) {
-    const char * keywords[] = {"size", "root", NULL};
-
-    int width = 1280;
-    int height = 720;
-    const char * root = NULL;
-
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|(ii)z", (char **)keywords, &width, &height, &root)) {
-        return NULL;
-    }
-
-    Window * res = PyObject_New(Window, Window_type);
-    res->size = Py_BuildValue("(ii)", width, height);
-    res->aspect = PyFloat_FromDouble((double)width / (double)height);
-    res->mouse = Py_BuildValue("(ii)", 0, 0);
-    res->render = NULL;
-    res->time = 0.0;
-    res->mouse_x = 0;
-    res->mouse_y = 0;
-    res->mouse_dx = 0;
-    res->mouse_dy = 0;
-    res->frame = 0;
-
-    PyObject * handler = PyObject_GetAttrString((PyObject *)res, "update");
-    res->window = zengl_create_window(width, height, handler, root);
-    zengl_make_current(res->window);
-    return res;
-}
-
-static PyObject * Window_meth_make_current(Window * self, PyObject * args) {
-    zengl_make_current(self->window);
-    Py_RETURN_NONE;
-}
-
-static PyObject * Window_meth_update(Window * self, PyObject * args) {
-    int mouse[4];
-    zengl_mouse_state(self->window, mouse);
-    self->time = zengl_time_state(self->window);
-    Py_SETREF(self->mouse, Py_BuildValue("(ii)", mouse[0], mouse[1]));
-    Py_SETREF(self->mouse_delta, Py_BuildValue("(ii)", mouse[2], mouse[3]));
-    if (self->render) {
-        Py_XDECREF(PyObject_CallObject(self->render, NULL));
-        if (PyErr_Occurred()) {
-            return NULL;
-        }
-       self->frame += 1;
-    }
-    Py_RETURN_NONE;
-}
-
-static PyObject * Window_meth_key_pressed(Window * self, PyObject * arg) {
-    if (!PyUnicode_CheckExact(arg)) {
-        return NULL;
-    }
-    int flags = zengl_key_state(self->window, PyUnicode_AsUTF8(arg));
-    if ((flags & 1) && (~flags & 2)) {
-        Py_RETURN_TRUE;
-    } else {
-        Py_RETURN_FALSE;
-    }
-}
-
-static PyObject * Window_meth_key_down(Window * self, PyObject * arg) {
-    if (!PyUnicode_CheckExact(arg)) {
-        return NULL;
-    }
-    int flags = zengl_key_state(self->window, PyUnicode_AsUTF8(arg));
-    if (flags & 1) {
-        Py_RETURN_TRUE;
-    } else {
-        Py_RETURN_FALSE;
-    }
-}
-
-static PyObject * Window_meth_key_release(Window * self, PyObject * arg) {
-    if (!PyUnicode_CheckExact(arg)) {
-        return NULL;
-    }
-    int flags = zengl_key_state(self->window, PyUnicode_AsUTF8(arg));
-    if ((~flags & 1) & (flags & 2)) {
-        Py_RETURN_TRUE;
-    } else {
-        Py_RETURN_FALSE;
-    }
-}
-
-static PyObject * Window_meth_render(Window * self, PyObject * arg) {
-    Py_INCREF(arg);
-    Py_XSETREF(self->render, arg);
-    Py_INCREF(arg);
-    return arg;
-}
-
 static void default_dealloc(PyObject * self) {
     Py_TYPE(self)->tp_free(self);
 }
@@ -741,33 +704,40 @@ static PyMethodDef Window_methods[] = {
     {"key_pressed", (PyCFunction)Window_meth_key_pressed, METH_O},
     {"key_down", (PyCFunction)Window_meth_key_down, METH_O},
     {"key_release", (PyCFunction)Window_meth_key_release, METH_O},
-    {"make_current", (PyCFunction)Window_meth_make_current, METH_NOARGS},
-    {"update", (PyCFunction)Window_meth_update, METH_NOARGS},
-    {"render", (PyCFunction)Window_meth_render, METH_O},
-    {"load_opengl_function", (PyCFunction)load_opengl_function, METH_O},
     {NULL},
 };
 
-static PyMemberDef Window_members[] = {
-    {"size", T_OBJECT, offsetof(Window, size), READONLY},
-    {"aspect", T_OBJECT, offsetof(Window, aspect), READONLY},
-    {"mouse", T_OBJECT, offsetof(Window, mouse), READONLY},
-    {"time", T_DOUBLE, offsetof(Window, time), READONLY},
-    {"frame", T_INT, offsetof(Window, frame), READONLY},
+static PyGetSetDef Window_getset[] = {
+    {"size", (getter)Window_get_size, NULL},
+    {"aspect", (getter)Window_get_aspect, NULL},
+    {"mouse", (getter)Window_get_mouse, NULL},
+    {"time", (getter)Window_get_time, NULL},
+    {"frame", (getter)Window_get_frame, NULL},
     {NULL},
 };
 
 static PyType_Slot Window_slots[] = {
     {Py_tp_methods, Window_methods},
-    {Py_tp_members, Window_members},
+    {Py_tp_getset, Window_getset},
     {Py_tp_dealloc, default_dealloc},
     {0},
 };
 
-static PyType_Spec Window_spec = {"mymodule.Window", sizeof(Window), 0, Py_TPFLAGS_DEFAULT, Window_slots};
+static PyMethodDef DefaultLoader_methods[] = {
+    {"load_opengl_function", (PyCFunction)load_opengl_function, METH_O},
+    {NULL},
+};
+
+static PyType_Slot DefaultLoader_slots[] = {
+    {Py_tp_methods, DefaultLoader_methods},
+    {Py_tp_dealloc, default_dealloc},
+    {0},
+};
+
+static PyType_Spec Window_spec = {"webgl.Window", sizeof(Window), 0, Py_TPFLAGS_DEFAULT, Window_slots};
+static PyType_Spec DefaultLoader_spec = {"webgl.DefaultLoader", sizeof(DefaultLoader), 0, Py_TPFLAGS_DEFAULT, DefaultLoader_slots};
 
 static PyMethodDef module_methods[] = {
-    {"window", (PyCFunction)meth_window, METH_VARARGS | METH_KEYWORDS},
     {NULL},
 };
 
@@ -776,6 +746,14 @@ static PyModuleDef module_def = {PyModuleDef_HEAD_INIT, "webgl", NULL, -1, modul
 extern PyObject * PyInit_webgl() {
     PyObject * module = PyModule_Create(&module_def);
     Window_type = (PyTypeObject *)PyType_FromSpec(&Window_spec);
+    DefaultLoader_type = (PyTypeObject *)PyType_FromSpec(&DefaultLoader_spec);
+
+    Window * window = PyObject_New(Window, Window_type);
+    PyModule_AddObject(module, "window", (PyObject *)window);
+
+    PyObject * zengl_helper = PyImport_ImportModule("_zengl");
+    PyObject_SetAttrString(zengl_helper, "DefaultLoader", (PyObject *)DefaultLoader_type);
+
     lookup = PyDict_New();
     fn("glCullFace", impl_glCullFace);
     fn("glClear", impl_glClear);
