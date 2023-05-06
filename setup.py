0dc8c4d0803671037a4b42695e605e00fd2758a1
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-05-06 15:36:00 +0300

    remove c++ flags

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.platform.startswith('linux'):
     extra_compile_args = ['-Wno-write-strings', '-Wno-narrowing']
 
 if sys.platform.startswith('darwin'):
-    extra_compile_args = ['-std=c++11', '-Wno-writable-strings', '-Wno-c++11-narrowing']
+    extra_compile_args = ['-Wno-writable-strings']
 
 if os.getenv('ZENGL_COVERAGE'):
     extra_compile_args += ['-O0', '--coverage']
