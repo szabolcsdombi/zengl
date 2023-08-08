e65191b86f5f39acd9966217c8bc75abc97afe6d
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-08-08 23:09:40 +0300

    update glcontext in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,11 @@
-import sys
-
 import pytest
 import zengl
+from glcontext import egl
 
 
 @pytest.fixture
 def ctx():
-    if sys.platform.startswith('linux'):
-        from glcontext import headless
-        devices = headless.devices()
-        headless.init(device=next(x['device'] for x in devices if 'EGL_MESA_device_software' in x['extensions']))
-        ctx = zengl.context(headless)
-    else:
-        ctx = zengl.context(zengl.loader(headless=True))
+    ctx = zengl.context(egl.create_context(glversion=330, mode='standalone'))
     yield ctx
     ctx.release('all')
     ctx.release('shader_cache')
