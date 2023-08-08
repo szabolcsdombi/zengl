9e33654b96ec55b652714257720c93a5d8d90161
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-08-08 23:08:29 +0300

    format stub

--- zengl-stubs/__init__.pyi
+++ zengl-stubs/__init__.pyi
@@ -4,7 +4,11 @@ CullFace = Literal['front', 'back', 'front_and_back', 'none']
 Topology = Literal['points', 'lines', 'line_loop', 'line_strip', 'triangles', 'triangle_strip', 'triangle_fan']
 
 MinFilter = Literal[
-    'nearest', 'linear', 'nearest_mipmap_nearest', 'linear_mipmap_nearest', 'nearest_mipmap_linear',
+    'nearest',
+    'linear',
+    'nearest_mipmap_nearest',
+    'linear_mipmap_nearest',
+    'nearest_mipmap_linear',
     'linear_mipmap_linear',
 ]
 
@@ -16,41 +20,138 @@ StencilOp = Literal['zero', 'keep', 'replace', 'incr', 'decr', 'invert', 'incr_w
 Step = Literal['vertex', 'instance']
 
 BlendConstant = Literal[
-    'zero', 'one', 'src_color', 'one_minus_src_color', 'src_alpha', 'one_minus_src_alpha', 'dst_alpha',
-    'one_minus_dst_alpha', 'dst_color', 'one_minus_dst_color', 'src_alpha_saturate', 'constant_color',
-    'one_minus_constant_color', 'constant_alpha', 'one_minus_constant_alpha', 'src1_alpha', 'src1_color',
-    'one_minus_src1_color', 'one_minus_src1_alpha',
+    'zero',
+    'one',
+    'src_color',
+    'one_minus_src_color',
+    'src_alpha',
+    'one_minus_src_alpha',
+    'dst_alpha',
+    'one_minus_dst_alpha',
+    'dst_color',
+    'one_minus_dst_color',
+    'src_alpha_saturate',
+    'constant_color',
+    'one_minus_constant_color',
+    'constant_alpha',
+    'one_minus_constant_alpha',
+    'src1_alpha',
+    'src1_color',
+    'one_minus_src1_color',
+    'one_minus_src1_alpha',
 ]
 
 VertexFormatShort = Literal[
-    '2u1', '4u1', '2i1', '4i1', '2nu1', '4nu1', '2ni1', '4ni1', '2u2', '4u2', '2i2', '4i2', '2nu2', '4nu2', '2ni2',
-    '4ni2', '2h', '4h', '1f', '2f', '3f', '4f', '1u', '2u', '3u', '4u', '1i', '2i', '3i', '4i',
+    '2u1',
+    '4u1',
+    '2i1',
+    '4i1',
+    '2nu1',
+    '4nu1',
+    '2ni1',
+    '4ni1',
+    '2u2',
+    '4u2',
+    '2i2',
+    '4i2',
+    '2nu2',
+    '4nu2',
+    '2ni2',
+    '4ni2',
+    '2h',
+    '4h',
+    '1f',
+    '2f',
+    '3f',
+    '4f',
+    '1u',
+    '2u',
+    '3u',
+    '4u',
+    '1i',
+    '2i',
+    '3i',
+    '4i',
 ]
 
 VertexFormat = Literal[
-    'uint8x2', 'uint8x4', 'sint8x2', 'sint8x4', 'unorm8x2', 'unorm8x4', 'snorm8x2', 'snorm8x4', 'uint16x2', 'uint16x4',
-    'sint16x2', 'sint16x4', 'unorm16x2', 'unorm16x4', 'snorm16x2', 'snorm16x4', 'float16x2', 'float16x4', 'float32',
-    'float32x2', 'float32x3', 'float32x4', 'uint32', 'uint32x2', 'uint32x3', 'uint32x4', 'sint32', 'sint32x2',
-    'sint32x3', 'sint32x4',
+    'uint8x2',
+    'uint8x4',
+    'sint8x2',
+    'sint8x4',
+    'unorm8x2',
+    'unorm8x4',
+    'snorm8x2',
+    'snorm8x4',
+    'uint16x2',
+    'uint16x4',
+    'sint16x2',
+    'sint16x4',
+    'unorm16x2',
+    'unorm16x4',
+    'snorm16x2',
+    'snorm16x4',
+    'float16x2',
+    'float16x4',
+    'float32',
+    'float32x2',
+    'float32x3',
+    'float32x4',
+    'uint32',
+    'uint32x2',
+    'uint32x3',
+    'uint32x4',
+    'sint32',
+    'sint32x2',
+    'sint32x3',
+    'sint32x4',
 ]
 
 ImageFormat = Literal[
-    'r8unorm', 'rg8unorm', 'rgba8unorm', 'r8snorm', 'rg8snorm', 'rgba8snorm', 'r8uint', 'rg8uint',
-    'rgba8uint', 'r16uint', 'rg16uint', 'rgba16uint', 'r32uint', 'rg32uint', 'rgba32uint', 'r8sint', 'rg8sint',
-    'rgba8sint', 'r16sint', 'rg16sint', 'rgba16sint', 'r32sint', 'rg32sint', 'rgba32sint', 'r16float', 'rg16float',
-    'rgba16float', 'r32float', 'rg32float', 'rgba32float', 'rgba8unorm-srgb',
-    'depth16unorm', 'depth24plus', 'depth24plus-stencil8', 'depth32float',
+    'r8unorm',
+    'rg8unorm',
+    'rgba8unorm',
+    'r8snorm',
+    'rg8snorm',
+    'rgba8snorm',
+    'r8uint',
+    'rg8uint',
+    'rgba8uint',
+    'r16uint',
+    'rg16uint',
+    'rgba16uint',
+    'r32uint',
+    'rg32uint',
+    'rgba32uint',
+    'r8sint',
+    'rg8sint',
+    'rgba8sint',
+    'r16sint',
+    'rg16sint',
+    'rgba16sint',
+    'r32sint',
+    'rg32sint',
+    'rgba32sint',
+    'r16float',
+    'rg16float',
+    'rgba16float',
+    'r32float',
+    'rg32float',
+    'rgba32float',
+    'rgba8unorm-srgb',
+    'depth16unorm',
+    'depth24plus',
+    'depth24plus-stencil8',
+    'depth32float',
 ]
 
 Vec3 = Tuple[float, float, float]
 Viewport = Tuple[int, int, int, int]
 Bytes = bytes | Any
 
-
 class LayoutBinding(TypedDict, total=False):
     name: str
     binding: int
-
 
 class BufferResource(TypedDict, total=False):
     type: Literal['uniform_buffer']
@@ -58,7 +159,6 @@ class BufferResource(TypedDict, total=False):
     buffer: 'Buffer'
     offset: int
     size: int
-
 
 class SamplerResource(TypedDict, total=False):
     type: Literal['sampler']
@@ -76,7 +176,6 @@ class SamplerResource(TypedDict, total=False):
     compare_func: CompareFunc
     max_anisotropy: float
 
-
 class VertexBufferBinding(TypedDict, total=False):
     buffer: 'Buffer'
     format: VertexFormat
@@ -85,11 +184,9 @@ class VertexBufferBinding(TypedDict, total=False):
     stride: int
     step: Step
 
-
 class DepthSettings(TypedDict, total=False):
     write: bool
     func: CompareFunc
-
 
 class StencilFaceSettings(TypedDict, total=False):
     fail_op: StencilOp
@@ -100,12 +197,10 @@ class StencilFaceSettings(TypedDict, total=False):
     write_mask: int
     reference: int
 
-
 class StencilSettings(TypedDict, total=False):
     front: StencilFaceSettings
     back: StencilFaceSettings
     both: StencilFaceSettings
-
 
 class BlendSettings(TypedDict, total=False):
     enable: bool | int
@@ -114,13 +209,11 @@ class BlendSettings(TypedDict, total=False):
     src_alpha: BlendConstant
     dst_alpha: BlendConstant
 
-
 class Info(TypedDict):
     vendor: str
     renderer: str
     version: str
     glsl: str
-
 
 class Limits(TypedDict):
     max_uniform_buffer_bindings: int
@@ -131,7 +224,6 @@ class Limits(TypedDict):
     max_draw_buffers: int
     max_samples: int
 
-
 class ImageFace:
     image: 'Image'
     size: Tuple[int, int]
@@ -139,20 +231,22 @@ class ImageFace:
     color: bool
     def clear(self) -> None: ...
     def blit(
-        self, target: 'ImageFace', target_viewport: Viewport | None = None,
-        source_viewport: Viewport | None = None, filter: bool = True, srgb: bool | None = None) -> None: ...
-
+        self,
+        target: 'ImageFace',
+        target_viewport: Viewport | None = None,
+        source_viewport: Viewport | None = None,
+        filter: bool = True,
+        srgb: bool | None = None,
+    ) -> None: ...
 
 class ContextLoader(Protocol):
     def load_opengl_function(name: str) -> int: ...
-
 
 class Buffer:
     size: int
     def write(self, data: Bytes, offset: int = 0) -> None: ...
     def map(self, size: int | None = None, offset: int | None = None, discard: bool = False) -> memoryview: ...
     def unmap(self) -> None: ...
-
 
 class Image:
     size: Tuple[int, int]
@@ -162,14 +256,23 @@ class Image:
     def face(self, layer: int = 0, level: int = 0) -> ImageFace: ...
     def clear(self) -> None: ...
     def write(
-        self, data: Bytes, size: Tuple[int, int] | None = None,
-        offset: Tuple[int, int] | None = None, layer: int | None = None, level: int = 0) -> None: ...
+        self,
+        data: Bytes,
+        size: Tuple[int, int] | None = None,
+        offset: Tuple[int, int] | None = None,
+        layer: int | None = None,
+        level: int = 0,
+    ) -> None: ...
     def mipmaps(self) -> None: ...
     def read(self, size: Tuple[int, int] | None = None, *, offset: Tuple[int, int] | None = None) -> bytes: ...
     def blit(
-        self, target: 'Image' | None = None, target_viewport: Viewport | None = None,
-        source_viewport: Viewport | None = None, filter: bool = True, srgb: bool | None = None) -> None: ...
-
+        self,
+        target: 'Image' | None = None,
+        target_viewport: Viewport | None = None,
+        source_viewport: Viewport | None = None,
+        filter: bool = True,
+        srgb: bool | None = None,
+    ) -> None: ...
 
 class Pipeline:
     vertex_count: int
@@ -178,7 +281,6 @@ class Pipeline:
     viewport: Viewport
     uniforms: Dict[str, memoryview] | None
     def render(self) -> None: ...
-
 
 class Context:
     info: Info
@@ -189,12 +291,20 @@ class Context:
     frame_time: int
     screen: int
     def buffer(
-        self, data: Bytes | None = None, size: int | None = None, dynamic: bool = False,
-        external: int = 0) -> Buffer: ...
+        self, data: Bytes | None = None, size: int | None = None, dynamic: bool = False, external: int = 0
+    ) -> Buffer: ...
     def image(
-        self, size: Tuple[int, int], format: ImageFormat, data: Bytes | None = None,
-        samples: int = 1, array: int = 0, levels: int = 1, texture: bool | None = None,
-        cubemap: bool = False, external: int = 0) -> Image: ...
+        self,
+        size: Tuple[int, int],
+        format: ImageFormat,
+        data: Bytes | None = None,
+        samples: int = 1,
+        array: int = 0,
+        levels: int = 1,
+        texture: bool | None = None,
+        cubemap: bool = False,
+        external: int = 0,
+    ) -> Image: ...
     def pipeline(
         self,
         vertex_shader: str = ...,
@@ -218,18 +328,25 @@ class Context:
         uniform_data: memoryview | None = None,
         viewport_data: memoryview | None = None,
         render_data: memoryview | None = None,
-        includes: Dict[str, str] | None = None) -> Pipeline: ...
+        includes: Dict[str, str] | None = None,
+    ) -> Pipeline: ...
     def new_frame(self, reset: bool = True, frame_time: bool = False) -> None: ...
     def end_frame(self, clean: bool = True, flush: bool = True, sync: bool = False) -> None: ...
     def release(self, obj: Buffer | Image | Pipeline | Literal['shader_cache'] | Literal['all']) -> None: ...
 
-
 def context(loader: ContextLoader | None = None) -> Context: ...
 def inspect(self, obj: Buffer | Image | Pipeline): ...
 def camera(
-    eye: Vec3, target: Vec3, up: Vec3 = (0.0, 0.0, 1.0),
-    fov: float = 45.0, aspect: float = 1.0, near: float = 0.1, far: float = 1000.0,
-    size: float = 1.0, clip: bool = False) -> bytes: ...
+    eye: Vec3,
+    target: Vec3,
+    up: Vec3 = (0.0, 0.0, 1.0),
+    fov: float = 45.0,
+    aspect: float = 1.0,
+    near: float = 0.1,
+    far: float = 1000.0,
+    size: float = 1.0,
+    clip: bool = False,
+) -> bytes: ...
 def bind(buffer: Buffer | None, layout: str, *attributes: Iterable[int]) -> List[VertexBufferBinding]: ...
 def calcsize(layout: str) -> int: ...
 def loader(headless: bool = False) -> ContextLoader: ...
