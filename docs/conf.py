cfd10805ef34a213b94a0149ed849064074653ce
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-10-14 14:44:32 +0300

    update docs :books:

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-project = 'zengl'
-copyright = '2022, Szabolcs Dombi'
+project = 'ZenGL'
+copyright = '2023, Szabolcs Dombi'
 author = 'Szabolcs Dombi'
 
 release = '1.16.0'
@@ -8,8 +8,16 @@ extensions = []
 templates_path = []
 exclude_patterns = []
 
+html_title = f'ZenGL {release}'
 html_theme = 'furo'
 html_static_path = ['_static']
+html_theme_options = {
+    'source_repository': 'https://github.com/szabolcsdombi/zengl/',
+    'source_branch': 'main',
+    'source_directory': 'docs/',
+    'light_logo': 'logo.png',
+    'dark_logo': 'logo.png',
+}
 
 
 def setup(app):
