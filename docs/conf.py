465f6f2ce6b044663f22f32305b48ffe4765b30f
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-01-29 20:43:35 +0200

    use single quotes for docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,24 @@
-project = "ZenGL"
-copyright = "2024, Szabolcs Dombi"
-author = "Szabolcs Dombi"
+project = 'ZenGL'
+copyright = '2024, Szabolcs Dombi'
+author = 'Szabolcs Dombi'
 
-release = "2.3.0"
+release = '2.3.0'
 
 extensions = []
 templates_path = []
 exclude_patterns = []
 
-html_title = f"ZenGL {release}"
-html_theme = "furo"
-html_static_path = ["_static"]
+html_title = f'ZenGL {release}'
+html_theme = 'furo'
+html_static_path = ['_static']
 html_theme_options = {
-    "source_repository": "https://github.com/szabolcsdombi/zengl/",
-    "source_branch": "main",
-    "source_directory": "docs/",
-    "light_logo": "logo.png",
-    "dark_logo": "logo.png",
+    'source_repository': 'https://github.com/szabolcsdombi/zengl/',
+    'source_branch': 'main',
+    'source_directory': 'docs/',
+    'light_logo': 'logo.png',
+    'dark_logo': 'logo.png',
 }
 
 
 def setup(app):
-    app.add_css_file("css/custom.css")
+    app.add_css_file('css/custom.css')
