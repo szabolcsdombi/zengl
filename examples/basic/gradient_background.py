abc177d3caa15e8c034c7893df3217443ef0c114
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-02-25 16:08:40 +0200

    remove unused function

--- examples/basic/gradient_background.py
+++ examples/basic/gradient_background.py
@@ -54,12 +54,6 @@ pipeline = ctx.pipeline(
             return fract((p3.x + p3.y) * p3.z);
         }
 
-        vec3 hash32(vec2 p) {
-            vec3 p3 = fract(vec3(p.xyx) * vec3(0.1031, 0.1030, 0.0973));
-            p3 += dot(p3, p3.yxz + 33.33);
-            return fract((p3.xxy + p3.yzz) * p3.zyx);
-        }
-
         void main() {
             vec2 uv = gl_FragCoord.xy / size;
             float c = distance(uv, vec2(0.5)) / sqrt(2.0);
