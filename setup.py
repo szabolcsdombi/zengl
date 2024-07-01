bab219df62fb47268a05884cdb8dd15644aa376f
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-07-01 11:56:46 +0300

    Drop support for unstable abi3 builds

--- setup.py
+++ setup.py
@@ -4,20 +4,9 @@ import sys
 
 from setuptools import Extension, setup
 
-py_limited_api = True
 extra_compile_args = []
 extra_link_args = []
 define_macros = []
-cmdclass = {}
-
-stubs = {
-    'packages': ['zengl-stubs'],
-    'package_data': {'zengl-stubs': ['__init__.pyi']},
-    'include_package_data': True,
-}
-
-if sys.hexversion < 0x030B0000:
-    py_limited_api = False
 
 if sys.platform.startswith('darwin'):
     extra_compile_args += ['-Wno-writable-strings']
@@ -26,9 +15,6 @@ if os.getenv('PYODIDE'):
     with open('zengl.js') as zengl_js:
         code = re.sub(r'\s+', ' ', zengl_js.read())
         define_macros += [('EXTERN_GL', f'"{code}"')]
-
-    py_limited_api = False
-    stubs = {}
 
 if os.getenv('ZENGL_COVERAGE'):
     extra_compile_args += ['-O0', '--coverage']
@@ -50,28 +36,12 @@ if os.getenv('ZENGL_WARNINGS'):
             '-fsanitize=undefined',
         ]
 
-if py_limited_api:
-    from wheel.bdist_wheel import bdist_wheel
-
-    class bdist_wheel_abi3(bdist_wheel):
-        def get_tag(self):
-            python, abi, plat = super().get_tag()
-
-            if python.startswith('cp'):
-                return 'cp311', 'abi3', plat
-
-            return python, abi, plat
-
-    cmdclass = {'cmdclass': {'bdist_wheel': bdist_wheel_abi3}}
-    define_macros += [('Py_LIMITED_API', 0x030B0000)]
-
 ext = Extension(
     name='zengl',
     sources=['zengl.c'],
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
     define_macros=define_macros,
-    py_limited_api=py_limited_api,
 )
 
 with open('README.md') as readme:
@@ -102,6 +72,7 @@ setup(
         'shader',
         'gpu',
     ],
-    **cmdclass,
-    **stubs,
+    packages=['zengl-stubs'],
+    package_data={'zengl-stubs': ['__init__.pyi']},
+    include_package_data=True,
 )
